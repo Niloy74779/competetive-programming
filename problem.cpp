100243ac1f695bec468ddcad92ce6fc14dd390e4
Niloy74779 <mdniloy74779@gmail.com>
2024-07-18 12:46:51 +0600

    Array problem

--- problem.cpp
+++ problem.cpp
@@ -11,14 +11,22 @@ int main()
 {
     fraction();
     optimize();
-    int n;
+    
+    int n,p=0,s=0;
     cin >> n;
-    long long int arr[100];
-    arr[1] = 0, arr[2] = 1;
-    for(int i = 3; i<=n; i++){
-        arr[i] = arr[i-1] + arr[i-2];
+    int arr[n][n];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> arr[i][j];
+        }
     }
-    cout << arr[n] << endl;
+
+    for(int i=0; i<n; i++){
+        p+=arr[i][i];
+        s += arr[i][n-i-1];
+    }
+    int x = abs(p-s);
+    cout << x << endl;
 
 	return 0;
 }
